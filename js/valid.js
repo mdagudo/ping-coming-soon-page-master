fd1902c50c07db6f0935f74cb4b01e13ce0e71f4
María Agudo <80266531+mdagudo@users.noreply.github.com>
2021-05-31 10:36:22 -0400

    added another condition

--- js/valid.js
+++ js/valid.js
@@ -1,6 +1,7 @@
 function validation() {
     var email = document.getElementById('email').value; // We save the email given by the user in a variable
     var error = document.getElementById('error'); // We save the error div in a variable
+    var empty = document.getElementById('empty'); // We save the empty div in a variable
 
     // We create a variable with the valid email format
     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -9,8 +10,13 @@ function validation() {
     if (email.match(mailformat)) {
         alert("Valid email address!");
         return true;
-    }
-    else { // If the email give isn't valid, we change the display of the error div and style the border of the email input to red
+        
+    } else if (email == '') {
+        empty.style.display="block";
+        document.getElementById('email').style.borderColor = 'red';
+        return false;
+
+    } else { 
         error.style.display="block";
         document.getElementById('email').style.borderColor = 'red';
         return false;
